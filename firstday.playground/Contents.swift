6c2e410f61a737d51aca658b97558e229b841bb7
Levent İrdem <leventirdem@gmail.com>
2023-02-18 02:41:43 +0300

    variables

---
+++ firstday.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+var str = "Hello,playground"
+
+str = "Goodbye"
+
+var age = 26
+
+var population = 8_000_000
+
+// Multi-line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Double
+
+var pi = 3.141
+
+//Bool
+
+var awesome = true
+
+var swiftIsHardToLearn = false
+
+// String interpolation
+
+var score = 85
+
+var str3 = "Your score is \(score)"
+
+var result = "The test result is : \(score)"
+
+// constants
+
+let capitalOfTurkey = "Ankara"
+
+let piNumber = 3.14
+
+
+// Type annotations
+
+let album:String = "Reputation"
+
+let year:Int = 1989
+
+let height:Double = 1.78
+
+let loveCoding:Bool = true
+
+
+// First day done.
